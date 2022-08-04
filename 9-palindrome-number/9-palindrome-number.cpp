ebe099217a14ef3fe3dc5b391478617213eef413
G Gautham Krishna <79567380+gautham227@users.noreply.github.com>
2022-08-04 15:37:18 +0530

    Time: 24 ms (46.93%), Space: 5.8 MB (74.58%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0)return false;
+        string s=to_string(x);
+        int l=0,r=s.size()-1;
+        while(l<r){
+            if(s[l]!=s[r]){
+                return false;
+            }
+            l++;r--;
+        }
+        return true;
+    }
+};
