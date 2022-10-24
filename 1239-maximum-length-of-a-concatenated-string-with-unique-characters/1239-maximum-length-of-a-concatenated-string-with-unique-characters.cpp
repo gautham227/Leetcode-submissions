d24266318b1346ed877993d475d32f55ba68a2fb
G Gautham Krishna <79567380+gautham227@users.noreply.github.com>
2022-10-24 22:46:22 +0530

    Time: 9 ms (88.33%), Space: 8.2 MB (92.84%) - LeetHub

---
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    
+    int recur(int ind, vector<int>& cur, int carry){
+        if(ind==cur.size()){
+            return 0;
+        }
+        if((carry&cur[ind])==0){
+            return max(recur(ind+1,cur,carry),recur(ind+1,cur,carry|cur[ind])+__builtin_popcount(cur[ind]));
+        }
+        return recur(ind+1,cur,carry);
+    }
+    
+    int maxLength(vector<string>& arr) {
+        vector<int> v;
+        int n=arr.size();
+        for(int i=0;i<n;i++){
+            int cur=0;
+            bool flag=true;
+            for(int j=0;j<arr[i].size();j++){
+                if((cur&(1<<(arr[i][j]-'a'))) ==0){
+                    cur|=(1<<(arr[i][j]-'a'));
+                }
+                else{
+                    flag=false;
+                }
+            }
+            if(flag)v.push_back(cur);
+        }
+        // for(int i=0;i<v.size();i++){
+        //     cout<<v[i]<<" ";
+        //     // cout<<__builtin_popcount(v[i])<<endl;
+        // }
+        cout<<endl;
+        return recur(0,v,0);
+    }
+};
