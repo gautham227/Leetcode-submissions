8322ad1a1dac8367af0627a22d7587ae11d28715
G Gautham Krishna <79567380+gautham227@users.noreply.github.com>
2022-12-28 01:49:33 +0530

    Time: 6 ms (26.69%), Space: 7.7 MB (11.68%) - LeetHub

--- 2511-maximum-enemy-forts-that-can-be-captured/2511-maximum-enemy-forts-that-can-be-captured.cpp
+++ 2511-maximum-enemy-forts-that-can-be-captured/2511-maximum-enemy-forts-that-can-be-captured.cpp
@@ -4,10 +4,11 @@ public:
         int ans=0;
 
         int n=forts.size();
+        int cur=0;
         for(int i=0;i<n;i++){
             if(forts[i]==1){
 
-                int cur=0;
+                cur=0;
                 for(int j=i+1;j<n;j++){
                     if(forts[j]==1)break;
                     else if(forts[j]==-1){
