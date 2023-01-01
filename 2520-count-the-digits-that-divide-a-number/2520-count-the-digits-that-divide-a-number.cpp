6b01a578e2f186bdae14a8e7c8c97b091c3e542d
G Gautham Krishna <79567380+gautham227@users.noreply.github.com>
2023-01-01 21:46:28 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (6.67%) - LeetHub

--- 2520-count-the-digits-that-divide-a-number/2520-count-the-digits-that-divide-a-number.cpp
+++ 2520-count-the-digits-that-divide-a-number/2520-count-the-digits-that-divide-a-number.cpp
@@ -2,14 +2,11 @@ class Solution {
 public:
     int countDigits(int num) {
         int num1=num;
-        vector<int> s;
-        while(num1>0){
-            s.push_back(num1%10);
-            num1/=10;
-        }
         int ans=0;
-        for(auto ele: s){
-            if(num%ele==0)ans++;
+
+        while(num1>0){
+            if(num%(num1%10)==0)ans++;
+            num1/=10;
         }
         return ans;
     }
