71ffb3f9359ed8c5f51e11570b2959992f430b47
G Gautham Krishna <79567380+gautham227@users.noreply.github.com>
2023-01-21 12:05:57 +0530

    Time: 0 ms (100.00%), Space: 7 MB (24.93%) - LeetHub

--- 93-restore-ip-addresses/93-restore-ip-addresses.cpp
+++ 93-restore-ip-addresses/93-restore-ip-addresses.cpp
@@ -4,7 +4,6 @@ public:
     set<string> ans;
     
     void recur(string& s, string& cur, int ind, int used, int muse, int len, int num, int type){
-        // cout<<ind<<" "<<num<<endl;
         if(num>255 || ind>len)return;
         if(used>muse)return;
         if(ind==len){
@@ -21,27 +20,6 @@ public:
             cur.push_back(s[ind]);
             if(s[ind]!='0' || ind==len-1)recur(s,cur,ind+1,used+1,muse,len,s[ind]-'0',0);
             else{
-            //     int cnt=0;
-            //     while(ind+1+cnt<len && s[ind+cnt+1]=='0'){
-            //         cur.push_back('.');
-            //         cur.push_back(s[ind+cnt+1]);
-            //         // recur(s,cur,ind+2,used+2,muse,len,s[ind+1]-'0');
-            //         // cur.pop_back();
-            //         // cur.pop_back();
-            //         cnt++;
-            //     }
-            //     if(used+cnt+1>muse)return;
-            //     if(ind+cnt+1<len){
-            //         cur.push_back('.');
-            //         cur.push_back(s[ind+cnt+1]);
-            //         recur(s,cur,ind+cnt+2,used+cnt+2,muse,len,s[ind+cnt+1]-'0',0)   ;
-            //         cur.pop_back();
-            //         cur.pop_back();
-            //     }
-            //     while(cnt--){
-            //         cur.pop_back();
-            //         cur.pop_back();
-            //     }
                 recur(s,cur,ind+1,used+1,muse,len,0,1);
             }  
             cur.pop_back();
