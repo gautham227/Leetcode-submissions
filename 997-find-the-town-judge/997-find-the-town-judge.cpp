a8508cd46a2e355b947b1f4db3a342c831517e45
G Gautham Krishna <79567380+gautham227@users.noreply.github.com>
2023-01-23 16:53:06 +0530

    Time: 592 ms (5.01%), Space: 68.9 MB (29.65%) - LeetHub

---
+++ 997-find-the-town-judge/997-find-the-town-judge.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        vector<int >v(n+1,0);
+        for(auto ele:trust){
+            v[ele[0]]--;
+            v[ele[1]]++;
+        }
+        for(int i=1;i<n+1;i++){
+            if(v[i]==n-1)return i;
+        }
+        return -1;
+    }
+};
