868628ae9393efcb31c2b3d6c413c5443cee25b6
G Gautham Krishna <79567380+gautham227@users.noreply.github.com>
2023-03-24 21:41:06 +0530

    Time: 794 ms (14.32%), Space: 175.1 MB (9.60%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     
-    int dfs(int cur, vector<set<vector<int > > >& adj, int par){
+    int dfs(int cur, vector<vector<vector<int > > >& adj, int par){
         int ans=0;
         for(auto ele: adj[cur]){
             if(ele[0]==par)continue;
@@ -12,10 +12,10 @@ public:
     }
     
     int minReorder(int n, vector<vector<int>>& connections) {
-        vector<set<vector<int> > >adj(n);
+        vector<vector<vector<int> > >adj(n);
         for(auto ele: connections){
-            adj[ele[0]].insert({ele[1],0});
-            adj[ele[1]].insert({ele[0],1});
+            adj[ele[0]].push_back({ele[1],0});
+            adj[ele[1]].push_back({ele[0],1});
         }
         return n-1-dfs(0,adj,-1);
     }
