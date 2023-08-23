7e4a2d743595d17ac9fbcf1559074373506899f4
G Gautham Krishna <79567380+gautham227@users.noreply.github.com>
2023-08-23 14:54:34 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (7.46%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.cpp
+++ 0767-reorganize-string/0767-reorganize-string.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     string reorganizeString(string s) {
-        map<char, int> mp;
+        unordered_map<char, int> mp;
         int n=s.size();
         string ans = "";
         for(int i=0;i<n;i++){
