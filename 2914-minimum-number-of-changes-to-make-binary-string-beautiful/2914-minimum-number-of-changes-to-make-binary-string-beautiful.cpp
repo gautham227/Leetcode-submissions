c27ce1c71940c3d66cf3b1d6e1e31f6fe81bfd72
G Gautham Krishna <79567380+gautham227@users.noreply.github.com>
2023-10-29 10:11:49 +0530

    Time: 17 ms (42.86%), Space: 10.2 MB (100.00%) - LeetHub

---
+++ 2914-minimum-number-of-changes-to-make-binary-string-beautiful/2914-minimum-number-of-changes-to-make-binary-string-beautiful.cpp
@@ -0,0 +1,43 @@
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+#define all(x) (x).begin(),(x).end()
+#define srt(v) sort(v.begin(),v.end())
+#define rev(v) reverse(v.begin(),v.end())
+#define lb(v,x) lower_bound(v.begin(),v.end(),x)
+#define ub(v,x) upper_bound(v.begin(),v.end(),x)
+#define cpy(v2,v1) v2.assign(v1.begin(),v1.end())
+#define maxv(a) *max_element(a.begin(), a.end())
+#define minv(a) *min_element(a.begin(), a.end())
+#define ff first
+#define ss second
+#define endl "\n"
+
+//type definitions
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<ll> vll;
+
+//constants
+
+const long long int inf = 1e18;
+const int mod = 1000000007;
+#define pi 3.141592653589793238462
+
+class Solution {
+public:
+    int minChanges(string s) {
+        int ans=0;
+        for(int i=0;i<s.size()-1;i+=2){
+            if(s[i]!=s[i+1]){
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
