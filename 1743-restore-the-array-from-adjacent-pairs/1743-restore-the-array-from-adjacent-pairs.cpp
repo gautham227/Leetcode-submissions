0638d1c770fe9e438dea3cd001b507757e0ec0a3
G Gautham Krishna <79567380+gautham227@users.noreply.github.com>
2023-11-10 17:40:19 +0530

    Time: 684 ms (12.12%), Space: 170.5 MB (10.00%) - LeetHub

---
+++ 1743-restore-the-array-from-adjacent-pairs/1743-restore-the-array-from-adjacent-pairs.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    map<int, multiset<int> >adj;
+    void dfs(int ind, vector<int>& ans){
+        if(adj.find(ind)==adj.end())return;
+        for(auto ele: adj[ind]){
+            if(adj[ele].size()!=1){
+                ans.push_back(ele);
+                adj[ind].erase(adj[ind].find(ele));
+                adj[ele].erase(adj[ele].find(ind));
+                if(adj[ind].size()==0){
+                    adj.erase(adj.find(ind));
+                }
+                if(adj[ele].size()==0){
+                    adj.erase(adj.find(ele));
+                }
+                dfs(ele, ans);
+                break;
+            }
+            else if(adj[ele].size()==1 && adj.size()==2){
+                ans.push_back(ele);
+                adj[ind].erase(adj[ind].find(ele));
+                adj[ele].erase(adj[ele].find(ind));
+                if(adj[ind].size()==0){
+                    adj.erase(adj.find(ind));
+                }
+                if(adj[ele].size()==0){
+                    adj.erase(adj.find(ele));
+                }
+                return;
+            }
+        }
+    }
+    vector<int> restoreArray(vector<vector<int>>& adjacentPairs) {
+        adj.clear();
+        for(auto ele: adjacentPairs){
+            adj[ele[0]].insert(ele[1]);
+            adj[ele[1]].insert(ele[0]);
+        }
+        int st=adj.begin()->first;
+        for(auto &ele: adj){
+            if(ele.second.size()%2==1){
+                st=ele.first;
+            }
+        }
+        // cout<<st<<endl;
+        vector<int> ans;
+        ans.push_back(st);
+        
+        dfs(st, ans);
+        return ans;
+    }
+};
