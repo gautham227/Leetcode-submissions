eb04771e2dac6d6d72bacf08fe597e08111a76da
G Gautham Krishna <79567380+gautham227@users.noreply.github.com>
2023-11-20 03:17:43 +0530

    Time: 5 ms (39.09%), Space: 7.9 MB (84.68%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,18 +1,54 @@
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+#define all(x) (x).begin(),(x).end()
+#define srt(v) sort(v.begin(),v.end())
+#define rev(v) reverse(v.begin(),v.end())
+#define lb(v,x) lower_bound(v.begin(),v.end(),x)
+#define ub(v,x) upper_bound(v.begin(),v.end(),x)
+#define cpy(v2,v1) v2.assign(v1.begin(),v1.end())
+#define maxv(a) *max_element(a.begin(), a.end())
+#define minv(a) *min_element(a.begin(), a.end())
+#define ff first
+#define ss second
+#define endl "\n"
+
+//type definitions
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<ll> vll;
+
+
 class Solution {
 public:
-    int uniquePathsWithObstacles(vector<vector<int>>& v) {
-        int n=v.size(),m=v[0].size();
-        vector<vector<int> >dp(n,vector<int>(m,-1));
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(v[i][j]==1){dp[i][j]=0;continue;}
-                if(i==0 && j==0){dp[i][j]=1;continue;}
-                int u=0,l=0;
-                if(i-1>-1)u=dp[i-1][j];
-                if(j-1>-1)l=dp[i][j-1];
-                dp[i][j]=u+l;
+    int uniquePathsWithObstacles(vector<vector<int>>& grid) {
+        int m=grid.size();
+        int n=grid[0].size();
+        vector<ll> prev(n+1,0), cur(n+1,0);
+        for(int i=n-1;i>=0;i--){
+            if(grid[m-1][i]==1){
+                break;
             }
+            prev[i]=1;
         }
-        return dp[n-1][m-1];
+        // debug(prev)
+        for(int i=m-2;i>=0;i--){
+            for(int j=n-1;j>=0;j--){
+                if(grid[i][j]==1){
+                    cur[j]=0;
+                }
+                else cur[j]=cur[j+1]+prev[j];
+            }
+            prev=cur;
+            // debug(prev)
+            fill(all(cur), 0);
+        }
+        return prev[0];
+
     }
 };
