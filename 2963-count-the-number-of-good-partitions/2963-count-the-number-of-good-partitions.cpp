848a5e0f23f75e5ccd4bf022c7f674900cc61129
G Gautham Krishna <79567380+gautham227@users.noreply.github.com>
2023-12-10 10:15:37 +0530

    Time: 347 ms (40.00%), Space: 137.4 MB (60.00%) - LeetHub

--- 2963-count-the-number-of-good-partitions/2963-count-the-number-of-good-partitions.cpp
+++ 2963-count-the-number-of-good-partitions/2963-count-the-number-of-good-partitions.cpp
@@ -5,19 +5,19 @@ ll binexp(ll a, ll b, ll mod) {ll res = 1; while (b > 0) {if (b & 1)res = (res *
 class Solution {
 public:
     int numberOfGoodPartitions(vector<int>& nums) {
-        map<int, vector<int> >pos;
+        map<int, int>pos;
         for(int i=0;i<nums.size();i++){
-            pos[nums[i]].push_back(i);
+            pos[nums[i]]=i;
         }
         int cnt=0;
         int ind=0;
         int last=0;
         while(ind!=nums.size()){
             while(ind<=last){
-                last=max(last, pos[nums[ind]].back());
+                last=max(last, pos[nums[ind]]);
                 ind++;
             }
-            if(ind!=nums.size())last=max(last, pos[nums[ind]].back());
+            if(ind!=nums.size())last=max(last, pos[nums[ind]]);
             cnt++;
         }
         return binexp(2,cnt-1,1000000000+7);
