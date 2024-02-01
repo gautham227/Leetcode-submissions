311ea6fd5b51458cbf6f6497ab7b3df803a326f7
G Gautham Krishna <79567380+gautham227@users.noreply.github.com>
2024-02-01 10:24:32 +0530

    Time: 135 ms (97.32%), Space: 119.7 MB (38.96%) - LeetHub

--- 2966-divide-array-into-arrays-with-max-difference/2966-divide-array-into-arrays-with-max-difference.cpp
+++ 2966-divide-array-into-arrays-with-max-difference/2966-divide-array-into-arrays-with-max-difference.cpp
@@ -4,13 +4,14 @@ public:
         vector<vector<int> >ans;
         sort(nums.begin(), nums.end());
         int n=nums.size();
+        vector<int> cur;
         for(int i=0;i<n;i+=3){
-            vector<int> cur;
             for(int j=i;j<i+3;j++){
                 cur.push_back(nums[j]);
             }
             if(cur[2]-cur[0]>k)return {};
             ans.push_back(cur);
+            cur.clear();
         }
         return ans;
     }
