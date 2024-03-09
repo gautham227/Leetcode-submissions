541f4f471acbe1380388cc8c29119c3d1728bb6f
G Gautham Krishna <79567380+gautham227@users.noreply.github.com>
2024-03-09 11:55:04 +0530

    Time: 68 ms (69.61%), Space: 52.9 MB (73.96%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.cpp
+++ 2540-minimum-common-value/2540-minimum-common-value.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
     int getCommon(vector<int>& nums1, vector<int>& nums2) {
-        set<int> s1, s2;
-        for(auto ele: nums1)s1.insert(ele);
-        for(auto ele: nums2)s2.insert(ele);
-        for(auto ele: s1){
-            if(s2.find(ele)!=s2.end()){
-                return ele;
-            }
+        int ind1=0, ind2=0;
+        while(ind1<nums1.size() && ind2<nums2.size()){
+            if(nums1[ind1]==nums2[ind2])return nums1[ind1];
+            if(nums1[ind1]>nums2[ind2])ind2++;
+            else ind1++;
         }
         return -1;
     }
