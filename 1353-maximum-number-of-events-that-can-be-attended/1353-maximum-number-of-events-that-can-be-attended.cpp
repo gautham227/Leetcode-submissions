be7ae92269d5956232d3b844a89e9b62c30d657a
G Gautham Krishna <79567380+gautham227@users.noreply.github.com>
2024-06-10 02:08:47 +0530

    Time: 209 ms (67.81%), Space: 90 MB (27.97%) - LeetHub

---
+++ 1353-maximum-number-of-events-that-can-be-attended/1353-maximum-number-of-events-that-can-be-attended.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    
+    static bool cmp(vector<int>& a, vector<int>& b){
+        return a[0]<b[0];
+    }
+    
+    int maxEvents(vector<vector<int>>& events) {
+        sort(events.begin(), events.end(), cmp);
+        multiset<int> ms;
+        int ans=0;
+        int ind=0;
+        int n=events.size();
+        for(int day=1;day<=100000;day++){
+            while(ind<n && events[ind][0]==day){
+                ms.insert(events[ind][1]);
+                ind++;
+            }
+            if(ms.size()>0){
+                ans++;
+                ms.erase(ms.begin());
+            }
+            while(ms.size()>0 && (*(ms.begin()))==day){
+                ms.erase(ms.begin());
+            }
+        }
+        
+        return ans;
+    }
+};
