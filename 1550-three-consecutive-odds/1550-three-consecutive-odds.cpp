2bec178611b8acb2d4768e8ff96c5186dd201081
G Gautham Krishna <79567380+gautham227@users.noreply.github.com>
2024-07-01 17:52:53 +0530

    Time: 3 ms (60.88%), Space: 10.6 MB (45.76%) - LeetHub

---
+++ 1550-three-consecutive-odds/1550-three-consecutive-odds.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool threeConsecutiveOdds(vector<int>& arr) {
+        for(int i=2;i<arr.size();i++){
+            if(arr[i]&1 && arr[i-1]&1 && arr[i-2]&1)return true;
+        }
+        return false;
+    }
+};
